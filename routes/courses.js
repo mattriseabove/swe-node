cc5772ba3dc261ca8b8abcb70631a628f74c8d1c
mattriseabove <mattriseabove@gmail.com>
2015-04-26 20:24:53 +0200

    auto format

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('courses', { title: 'todo: course page' });
     res.send('respond with a resource');
 });
 
